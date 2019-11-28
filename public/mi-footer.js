51445d4d859027dcbdf011d5a2a99eb9ccdfe610
AndresHuaracha <55821924+AndresHuaracha@users.noreply.github.com>
2019-11-28 14:02:08 -0600

    Delete mi-footer.js

--- public/mi-footer.js
+++
@@ -1,5 +0,0 @@
-customElements.define("mi-footer", class extends HTMLElement {
-  connectedCallback() {
-    this.innerText = "Copyright © 2019 Huaracha Rosales Andrés IC51-M.";
-  }
-}, { extends: "footer" });
